54c28e178cea34f07bdec0a51f952e57aebbbb9d
Ephraim Glick <eglick@ripple.com>
2021-04-07 09:26:51 -0700

    Initial hello world commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Build a Docker image:
+
+`docker build -t ephraimglick/ten-percent .`
+
+Run the application on port 8080:
+
+`docker run -p 8080:8080 ephraimglick/ten-percent`
---
+++ src/test/java/com/ephraimglick/tenpercent/TenPercentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ephraimglick.tenpercent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenPercentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
